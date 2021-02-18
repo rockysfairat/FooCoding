b37d2ab825900f06c9d2598dcf27fd6abe47ca6c
Oleksandr Yurchenko <rockysfairat@gmail.com>
2021-02-18 12:21:56 +0100

    code doesnt work

--- JavaScript1/Week 3/testing.js
+++ JavaScript1/Week 3/testing.js
@@ -1,30 +1,13 @@
+// 10 - Use the list of vehicles to write an advertisement.
+// So that it prints something like: "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.".
+// (Hint: use a for loop.)
+let text = 'Amazing Joe\'s Garage, we service cars, ';
+
 let listOfVehicles = ['motorbike', 'caravan', 'bike'];
 
-function vehicleNew1(colorNew, carTypeNew, ageNew) {
-    if (typeof colorNew == "string" && carTypeNew == 1 && ageNew == 0) {
-        return 'a ' + colorNew + ' ' + 'new ' + listOfVehicles[0];
-    } else if (typeof colorNew == "string" && carTypeNew == 2 && ageNew == 0) {
-        return 'a ' + colorNew + ' ' + 'new ' + listOfVehicles[1];
-    } else if (typeof colorNew == "string" && carTypeNew == 3 && ageNew == 0) {
-        return 'a ' + colorNew + ' ' + 'new ' + listOfVehicles[2];                 
-        // new vehicles block ends here
-    } else if (typeof colorNew == "string" && carTypeNew == 1 && ageNew >= 1) {
-        return 'a ' + colorNew + ' ' + 'used ' + listOfVehicles[0];
-    } else if (typeof colorNew == "string" && carTypeNew == 2 && ageNew >= 1) {
-        return 'a ' + colorNew + ' ' + 'used ' + listOfVehicles[1];
-    } else if (typeof colorNew == "string" && carTypeNew == 3 && ageNew >= 1) {
-        return 'a ' + colorNew + ' ' + 'used ' + listOfVehicles[2];
-    } else {
-        return undefined;
+function advert() {
+    for (let i = 0; i <= listOfVehicles.length; i++) { // THIS IS UNFINISHED!
+        return text + listOfVehicles[i] + 's, ';
     }
 }
-
-console.log(vehicleNew1('black', 3, 10));
-
-function vehicleNew (colorNew, carTypeNew, ageNew) {
-    let msg = '';
-    ageNew >= 1 ? msg = `An used ${colorNew} ${listOfVehicles[carTypeNew -1]}` : `A new ${colorNew} ${listOfVehicles[carTypeNew -1]}`;
-    return console.log(msg);
-}
-
-vehicleNew('blaccccccccccccccck', 1, 2);
+console.log(advert());
